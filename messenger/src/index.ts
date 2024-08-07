a3ce91bdce0e4d188aa5291c4ac98d1b6b6fbc06
Fox-Alice <elviratr@yandex.ru>
2024-08-07 14:00:14 +0300

    add login register page, routing

---
+++ messenger/src/index.ts
@@ -0,0 +1,3 @@
+export {Login} from './pages/login';
+export {Register} from './pages/register';
+export {Nav} from './pages/nav';
---
+++ messenger/src/main.ts
@@ -0,0 +1,34 @@
+import Handlebars from 'handlebars';
+import * as Components from './components/button';
+import * as Pages from './index';
+
+
+const pages = {
+  'login': [ Pages.Login, {test: 123} ],
+  'register': [Pages.Register],
+  'nav': [Pages.Nav]
+};
+
+Object.entries(Components).forEach(([name, component]) => {
+  Handlebars.registerPartial(name, component);
+});
+
+function navigate(page: string) {
+  const [source, context] = pages[page];
+  const container = document.getElementById('app');
+  console.log(container);
+  const templatingFunction = Handlebars.compile(source);
+  container.innerHTML = templatingFunction(context);
+}
+
+document.addEventListener('DOMContentLoaded', () => navigate('nav'));
+
+document.addEventListener('click', e => {
+  const page = e.target.getAttribute('page');
+  if (page) {
+    navigate(page);
+  e.preventDefault();
+  e.stopImmediatePropagation();
+};
+})
+
